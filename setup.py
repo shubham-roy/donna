532a47964e0ac8506d298e53100f657ede51fa6f
shubham_roy <srworkspace4@gmail.com>
2022-06-08 18:45:17 +0530

    Made initial setup related changes.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="donna",
+    version="0.0.1",
+    description="A CLI tool to work for you when you are with your PC.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Shubham Roy",
+    packages=find_packages(),
+    python_requires=">=3.10, <4",
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "donna=donna.cli:entry_point",
+        ],
+    },
+)
